03e3e680aab531934c96830c269e1870a1f29fe8
Andrew Baker <andrew_j_baker2@hotmail.com>
2019-08-13 22:18:25 +0100

    Use rollup-plugin-node-resolve for bundling

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,16 @@
 import htmlBundle from 'rollup-plugin-html-bundle'
+import nodeResolve from 'rollup-plugin-node-resolve'
 import serve from 'rollup-plugin-serve'
 import { terser } from 'rollup-plugin-terser'
 
-const fs = require('fs')
-const path = require('path')
-
 export default {
-  external: [
-    'kontra'
-  ],
   input: 'src/main.js',
   output: {
-    banner: fs.readFileSync(path.join(__dirname, 'node_modules/kontra/kontra.js'), 'utf-8'),
     file: 'dist/bundle.js',
-    format: 'iife',
-    globals: {
-      kontra: 'kontra'
-    }
+    format: 'iife'
   },
   plugins: [
+    nodeResolve(),
     terser(),
     htmlBundle({
       inline: true
